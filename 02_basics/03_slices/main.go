08860f4850e69d47dc451d5cacdcb93957f4773d
pool80 <pironho@yandex.ru>
2021-09-05 22:49:47 +0300

    Cлайсы

--- 02_basics/03_slices/main.go
+++ 02_basics/03_slices/main.go
@@ -6,13 +6,12 @@ func main() {
 	//из слайса с днями недели надо скопировать в новый слайс рабочие дни, а из исходного слайса удалить скопированные, чтобы остались только выходные дни.
 	//Выведите на экран слайсы с выходными и рабочими днями недели.
 	days := []string{"mon", "tue", "wen", "thu", "fri", "sat", "sun"}
-	//days := []int{1, 2, 3, 4, 5, 6, 7}
-	workDays := make([]string, 5, 7)
+	workDays := make([]string, 5)
 	copy(workDays, days)
 	days = append(days[5:7], days[7:]...)
 	fmt.Println(workDays, days)
 
 	//нужно объединить слайс с выходными днями и слайс с рабочими в один слайс. Выведите на экран итоговый слайс с днями.
 	week := append(workDays, days...)
-	fmt.Println(week)
+	fmt.Println(week, len(week))
 }
